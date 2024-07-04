a8af4cb916a16b3a1211f2180f95dbcd1573e171
QueilaAndreson <ovyaselvee@gmail.com>
2024-07-05 02:03:25 +0530

    landing page created

---
+++ landingPage.js
@@ -0,0 +1,4 @@
+
+const handleMenu = () => {
+    window.location.href = "menu.html";
+}
---
+++ menu.js
@@ -0,0 +1,60 @@
+const menuItems = [
+    {
+      title: "Smashed Avo",
+      image: "/images/smashed-avo.png",
+      price: 20,
+      id: 0,
+      quantity: 1,
+    },
+    {
+      title: "Ying & Yang",
+      image: "/images/ying-yang.png",
+      price: 20,
+      id: 1,
+      quantity: 1,
+    },
+    {
+      title: "Pancakes",
+      image: "/images/pancakes.png",
+      price: 20,
+      id: 2,
+      quantity: 1,
+    },
+    {
+      title: "Huevos Rancheros",
+      image: "/images/huevos-rancheros.png",
+      price: 20,
+      id: 3,
+      quantity: 1,
+    },
+    {
+      title: "Rancheros",
+      image: "/images/rancheros.png",
+      price: 20,
+      id: 4,
+      quantity: 1,
+    },
+    {
+      title: "Veg Burger",
+      image: "/images/burg-veg.png",
+      price: 20,
+      id: 5,
+      quantity: 1,
+    },
+    {
+      title: "Egg Burger",
+      image: "/images/burg-egg.png",
+      price: 20,
+      id: 6,
+      quantity: 1,
+    },
+    {
+      title: "Burrito",
+      image: "/images/burrito.png",
+      price: 20,
+      id: 7,
+      quantity: 1,
+    },
+  ];
+
+  const menuCardContainer = document.querySelector('.menu-card-container');
